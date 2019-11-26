c852f9938008388ecd5a85aaece62e269f881c1c
Alexander Simmons <41705368+AlexSimmonsFCC@users.noreply.github.com>
2019-11-26 14:39:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,83 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZmNjIiwiYSI6InBiaGMyLU0ifQ.LOmVYpUCFv2yWpbvxDdQNg';
 var map = new mapboxgl.Map({
 container: 'map',
-style: 'mapbox://styles/mapbox/light-v10',
-center: [-81.103, 35.581],
+style: 'mapbox://styles/mapbox/dark-v10',
+center: [-96.573, 39.397],
 minZoom: 3,
-zoom: 10
+zoom: 4
 });
  
 var zoomThreshold = 4;
 
 
 // Get DOM Objects 
-var filterButton = document.getElementById('filterButton')
-var resetButton = document.getElementById('resetButton')
+var filterButton = document.getElementById('filterButton');
+var resetButton = document.getElementById('resetButton');
+
+var popup = new mapboxgl.Popup();
+
+// Create Popup Windows
+map.on('click', "VerizonWireless",  function (e) {
+popup.setLngLat(e.lngLat)
+popup.setHTML("<h4>Provider:</h4>" + e.features[0].properties.DBA + "<br>" 
+         + e.features[0].properties.TECHNOLOGY + "<br>" 
+         + e.features[0].properties.MINUP + "<br>" 
+         + e.features[0].properties.MINDOWN + "<br>" 
+)
+popup.addTo(map);
+});
+
+map.on('click', "Carolina West Wireless",  function (e) {
+popup.setLngLat(e.lngLat)
+popup.setHTML("<h4>Provider:</h4>" + e.features[0].properties.DBA + "<br>" 
+         + e.features[0].properties.TECHNOLOGY + "<br>" 
+         + e.features[0].properties.MINUP + "<br>" 
+         + e.features[0].properties.MINDOWN + "<br>" 
+)
+popup.addTo(map);
+});
 
 
 // Create Filter Button Action
 filterButton.addEventListener('click', function(e){
-if (ProviderSelect.selectedIndex == 0 && TechnologySelect.selectedIndex == 0 && SpeedSelect.selectedIndex == 0){
+
+map.setLayoutProperty(ProviderSelect.value, "visibility", "visible");
+
+ if (ProviderSelect.value == 'Carolina West Wireless'){
+ map.flyTo({center: [ -81.357, 35.733], zoom: 7})
+}
+
+
+
+
+
+
+
+ if (ProviderSelect.selectedIndex == 0 && TechnologySelect.selectedIndex == 0 && SpeedSelect.selectedIndex == 0){
 map.setFilter('CarolinaWest');
 }
 else if (SpeedSelect.selectedIndex != 0){
-map.setFilter('CarolinaWest', ['==', 'MINUP', Number(SpeedSelect.value)]);
+map.setFilter(ProviderSelect.value, ['==', 'MINUP', Number(SpeedSelect.value)]);
   }
 else if (TechnologySelect.selectedIndex != 0){
-map.setFilter('CarolinaWest', ['==', 'TECHNOLOGY', Number(TechnologySelect.value)]);
+map.setFilter(ProviderSelect.value, ['==', 'TECHNOLOGY', Number(TechnologySelect.value)]);
   }
-  
+  else if (ProviderSelect.selectedIndex != 0){
+ map.setFilter(ProviderSelect.value, ['==', 'DBA', ProviderSelect.value]);
+  }  
+
 });
+
 
 
 // Create Reset Button Action
 resetButton.addEventListener('click', function(e){
-map.setFilter('CarolinaWest');
+map.setLayoutProperty(ProviderSelect.value, "visibility", "none");
 ProviderSelect.selectedIndex=0;
 TechnologySelect.selectedIndex=0;
 SpeedSelect.selectedIndex=0;
+popup.remove();
+map.flyTo({center: [-96.573, 39.397], zoom: 4})
 });
 
 
@@ -45,8 +87,8 @@ SpeedSelect.selectedIndex=0;
 var ProviderSelect = document.getElementById("ProviderSelect");
 var ProviderList = {
     'null' : 'Select a Provider...',
-    'Carolina West' : 'Carolina West',
-    'Verizon' : 'Verizon',
+    'Carolina West Wireless' : 'Carolina West Wireless',
+    'VerizonWireless' : 'Verizon',
     'Sprint < 400'  : 'Sprint',
     'T-Mobile'  : 'T-Mobile'
 }
@@ -89,35 +131,44 @@ map.on('load', function() {
  
 map.addSource('CarolinaWest', {
 'type': 'vector',
-'url': 'mapbox://fcc.39ptq2xg'
+'url': 'mapbox://fcc.d2qrpeux'
+});
+
+map.addSource('Verizon_EVDO_CELL', {
+'type': 'vector',
+'url': 'mapbox://fcc.102ha294'
 });
  
-map.addLayer({
-'id': 'CarolinaWest',
+ map.addLayer({
+'id': 'Carolina West Wireless',
 'source': 'CarolinaWest',
-'source-layer': 'CarolinaWest_All-c17pch',
+'source-layer': 'CarolinaWest_All2geojson',
 'type': 'fill',
 'paint': {
-'fill-color': '#f99',
-'fill-opacity': 0.4,
-'fill-outline-color': 'rgba(255, 0, 0, 1)'
+'fill-color': '#348feb',
+'fill-opacity': 0.6,
+'fill-outline-color': 'rgb(52, 143, 235)'
 },
+layout: {
+      visibility: "none"
+    },
 }); 
 
+ map.addLayer({
+'id': 'VerizonWireless',
+'source': 'Verizon_EVDO_CELL',
+'source-layer': 'VZW_EVDO_CELL2geojson',
+'type': 'fill',
+'paint': {
+'fill-color': '#ef1d1d',
+'fill-opacity': 0.4,
+'fill-outline-color': 'rgb(239, 29, 29)'
+},
+layout: {
+      visibility: "none"
+    },
+});   
 
- 
-// Create Popup Window
-map.on('click', 'CarolinaWest', function (e) {
-new mapboxgl.Popup()
-.setLngLat(e.lngLat)
-.setHTML("<h4>Provider:</h4>" + e.features[0].properties.DBA + "<br>" 
-         + e.features[0].properties.TECHNOLOGY + "<br>" 
-         + e.features[0].properties.MINUP + "<br>" 
-         + e.features[0].properties.MINDOWN + "<br>" 
-)
 
-.addTo(map);
-});
- 
 
-});
+}); 
